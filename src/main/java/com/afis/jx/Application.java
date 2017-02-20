24b3bcc1159ffa1453e7c97ffb70654fec669a85
chengen <chengen@auto-fis.com>
2017-02-20 16:18:43 +0800

    初始化

--- src/main/java/com/afis/jx/Application.java
+++ src/main/java/com/afis/jx/Application.java
@@ -7,12 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.afis.jx.ckfinder.connector.ConnectorServlet;
 
 @SpringBootApplication(scanBasePackages = { "com.afis.jx" })
-@EnableScheduling // 发现注解@Scheduled的任务并后台执行
 public class Application {
 	private static Logger logger = LoggerFactory.getLogger(Application.class);
 
@@ -25,7 +23,7 @@ public class Application {
 
 	@Bean
 	public ServletRegistrationBean servletRegistrationBean() {
-		return new ServletRegistrationBean(new ConnectorServlet(), "/ckfinder/core/*");// ServletName默认值为首字母小写，即myServlet
+		return new ServletRegistrationBean(new ConnectorServlet(), "/ckfinder/core/*");// ServletName默认值为首字母小写，即ConnectorServlet
 	}
 
 }
