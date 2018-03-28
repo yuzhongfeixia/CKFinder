778a17e3326fac74aba844df675dc90786aa56f6
chengen <>
2018-03-28 13:46:10 +0800

    代码优化

--- src/main/java/com/afis/jx/security/MyWebMvcConfigurerAdapter.java
+++ src/main/java/com/afis/jx/security/MyWebMvcConfigurerAdapter.java
@@ -7,13 +7,14 @@ import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.afis.jx.storage.StorageProperties;
 
-@Configuration
+@Component
 public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
 
 	// @Bean
--- src/main/java/com/afis/jx/security/MyWebSecurityConfigurerAdapter.java
+++ src/main/java/com/afis/jx/security/MyWebSecurityConfigurerAdapter.java
@@ -1,14 +1,14 @@
 package com.afis.jx.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
-import com.afis.jx.web.common.CoreConstants;
-
+import org.springframework.stereotype.Component;
+@Component
 public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -26,10 +26,12 @@ public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.headers().frameOptions().disable();
-//		http.csrf().disable().authorizeRequests().antMatchers("/css/**", "/js/**", "/img/**").permitAll().anyRequest()
-//				.hasAuthority(CoreConstants.OperatortType.ADMIN).and().formLogin().loginPage("/login.htm")
-//				.defaultSuccessUrl("/index.htm").successHandler(successHandler).loginProcessingUrl("/login").permitAll()
-//				.and().logout().deleteCookies(CookieConstants.WEB_USERNAME).permitAll();
+		http.csrf().disable().authorizeRequests().antMatchers("/css/**", "/js/**", "/img/**","/ckfinder_demo.htm").permitAll();
+		// http.csrf().disable().authorizeRequests().antMatchers("/css/**",
+		// "/js/**", "/img/**").permitAll().anyRequest()
+		// .hasAuthority(CoreConstants.OperatortType.ADMIN).and().formLogin().loginPage("/login.htm")
+		// .defaultSuccessUrl("/index.htm").successHandler(successHandler).loginProcessingUrl("/login").permitAll()
+		// .and().logout().deleteCookies(CookieConstants.WEB_USERNAME).permitAll();
 	}
 
 	@Autowired
